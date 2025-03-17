09b3d50bf2707ecfa457fda0983f71b183f0f076
Vishnu.S <siliverivishnu@gmail.com>
2025-03-17 12:25:59 +0530

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Program2{
+
+    public static void main(String[] args) {
+        Scanner hehe = new Scanner(System.in);
+        int myint = hehe.nextInt();
+        String Str1 = hehe.next();
+        double myDou= hehe.nextDouble();
+        String Str2 = hehe.next();
+        hehe.close();
+           System.out.println( "My int is "+ myint);
+            System.out.println("My string is "+ Str1);
+                System.out.println( "My double is"+myDou);
+                 System.out.println("My string is "+ Str2);
+
+
+       
+    }
+}
---
+++ Program3.java
@@ -0,0 +1,20 @@
+  import java.lang.*;
+  import java.util.scanner;
+
+  class Program3{
+	Scanner sc= new Scanner()
+
+    public static void main(){
+		system.out.println("enter the integers")
+		int a = sc.nextInt;
+		int b = sc.nextInt;
+ 		system.out.println(sum(a,b))
+	}
+
+    public static int sum(int x,int y){
+             return(x+y); 
+
+	}
+
+
+}
